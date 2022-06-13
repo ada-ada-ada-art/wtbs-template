0da111f3df88f5eec646ee8c157dec04099a1327
Ada Hyldahl Fogh <hello@ada-ada-ada.art>
2022-06-13 16:25:45 +0200

    Working on a version that includes multiple layers of letters

--- src/styles/ada-ada-ada.js
+++ src/styles/ada-ada-ada.js
@@ -13,12 +13,8 @@ addLetterStyle(class AdaAdaAda extends LetterStyle {
   static name = 'AdaAdaAda';
   isDebugging = true
 
-  coreHue = 100
-
   setup() {
     this._inverted = this.probability(0.5);
-
-    this.coreHue = this.pg.random(0, 360)
   }
 
   draw() {
@@ -35,9 +31,16 @@ addLetterStyle(class AdaAdaAda extends LetterStyle {
 
     this.pg.blendMode(this.pg.OVERLAY)
     this.drawFilmGradients()
-    this.pg.blendMode(this.pg.BLEND)
-    this.drawLetter()
 
+    let episodeCount = 25
+    for(let i = 0; i < episodeCount; i++) {
+      this.drawLetter('dejaVu', this.pg.DIFFERENCE)
+    }
+    this.drawLetter('dejaVu', this.pg.BLEND, 0)
+    this.pg.blendMode(this.pg.OVERLAY)
+    this.drawFilmGradients()
+
+    this.pg.blendMode(this.pg.BLEND)
     this.hasDrawnNoise = false
     if(!this.hasDrawnNoise){
         for(let i = 0; i < noiseLevel; i++) {
@@ -49,10 +52,6 @@ addLetterStyle(class AdaAdaAda extends LetterStyle {
 
   drawBackground() {
     const ctx = this.pg.drawingContext
-    // const gradient = ctx.createLinearGradient(
-    //     0, 0,
-    //     this.pg.width * 2, 0
-    // )
     let centerX = this.pg.width / 2
     let centerY = this.pg.height / 2
     const gradient = ctx.createRadialGradient(
@@ -60,10 +59,10 @@ addLetterStyle(class AdaAdaAda extends LetterStyle {
       centerX, centerY, this.pg.width * 2
     )
 
-    gradient.addColorStop(0, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
-    gradient.addColorStop(.33, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
-    gradient.addColorStop(.66, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
-    gradient.addColorStop(1, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
+    gradient.addColorStop(0, this.pg.color(this.pg.random(0, 360), 90, 50))
+    gradient.addColorStop(.33, this.pg.color(this.pg.random(0, 360), 90, 50))
+    gradient.addColorStop(.66, this.pg.color(this.pg.random(0, 360), 90, 50))
+    gradient.addColorStop(1, this.pg.color(this.pg.random(0, 360), 90, 50))
     
     this.pg.noStroke()
     this.pg.fill('transparent')
@@ -71,48 +70,36 @@ addLetterStyle(class AdaAdaAda extends LetterStyle {
     this.pg.rect(0, 0, this.pg.width, this.pg.height)
   }
 
-  drawLetter(fontName = 'dejaVu') {
+  drawLetter(fontName = 'dejaVu', blendMode = this.pg.BLEND, randomOffset = this.size * .1) {
     const f = this.fonts[fontName];
     this.pg.textFont(f.font);
     this.pg.textSize(this.size * f.sizeFactor);
     this.pg.textAlign(this.pg.CENTER, this.pg.CENTER);
-    let textBounds = f.font.textBounds(this.letter, 0, this.size * f.posFactor, this.size)
-    const ctx = this.pg.drawingContext
-    let isBlackWhite = false
-    const gradient = ctx.createLinearGradient(
-      textBounds.x, textBounds.y,
-      textBounds.x + textBounds.w * 2, textBounds.y
+    let letterPos = this.pg.createVector(
+      this.pg.random(-randomOffset, randomOffset),
+      (this.size * f.posFactor) + this.pg.random(-randomOffset, randomOffset)
     )
-    if (isBlackWhite) {
-      gradient.addColorStop(0, this.pg.color(0, 0, this.pg.random(0,100)))
-      gradient.addColorStop(.33, this.pg.color(0, 0, this.pg.random(0,100)))
-      gradient.addColorStop(.66, this.pg.color(0, 0, this.pg.random(0,100)))
-      gradient.addColorStop(1, this.pg.color(0, 0, this.pg.random(0,100)))
-    } else {
-      gradient.addColorStop(0, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
-      gradient.addColorStop(.33, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
-      gradient.addColorStop(.66, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
-      gradient.addColorStop(1, this.pg.color(this.coreHue + this.pg.random(-60, 60) % 360, 90, 50))
-    }
+    let textBounds = f.font.textBounds(this.letter, letterPos.x, letterPos.y, this.size)
+    const ctx = this.pg.drawingContext
+    const radialGradient = ctx.createRadialGradient(
+      textBounds.x + textBounds.w * .5, textBounds.y + textBounds.h * .5, 0.1,
+      textBounds.x + textBounds.w * .5, textBounds.y + textBounds.h * .5, textBounds.w * 2,
+    )
+    
+    radialGradient.addColorStop(0, this.pg.color(this.pg.random(0,360), 90, 50))
+    radialGradient.addColorStop(.33, this.pg.color(this.pg.random(0,360), 90, 50))
+    radialGradient.addColorStop(.66, this.pg.color(this.pg.random(0,360), 90, 50))
+    radialGradient.addColorStop(1, this.pg.color(this.pg.random(0,360), 90, 50))
     
     this.pg.noStroke()
-    this.pg.strokeWeight(3)
-    ctx.shadowColor = this.pg.color(0, 0, 70,1)
-    ctx.shadowOffsetX = 0
-    ctx.shadowOffsetY = 0
-    ctx.shadowBlur = 250
     this.pg.fill('transparent')
-    // ctx.fillStyle = gradient
-    this.pg.blendMode(this.pg.DARKEST)
-    this.pg.text(this.letter, 0, this.size * f.posFactor, this.size);
-    ctx.fillStyle = gradient
-    this.pg.blendMode(this.pg.BLEND)
-    ctx.shadowColor = 'transparent'
-    this.pg.text(this.letter, 0, this.size * f.posFactor, this.size);
+    ctx.fillStyle = radialGradient
+    this.pg.blendMode(blendMode)
+    this.pg.text(this.letter, letterPos.x, letterPos.y, this.size);
   }
 
   drawFilmGradients() {
-    const gradients = 10
+    const gradients = 5
     const ctx = this.pg.drawingContext
     this.pg.noStroke()
     for(let grad = 0; grad < gradients; grad++) {
@@ -129,56 +116,6 @@ addLetterStyle(class AdaAdaAda extends LetterStyle {
       ctx.fillStyle = gradient
       this.pg.rect(0, 0, this.pg.width, this.pg.height)
     }
-  }
-
-  drawScratch(placement) {
-    const gfx = this.createGraphics(this.pg.width * .2, this.pg.height * .2)
-    const scratchLength = this.pg.random(5, 10)
-    if (this.isDebugging) {
-      this.pg.noFill()
-      this.pg.stroke(240, 100, 50)
-      this.pg.rect(placement.x, placement.y, gfx.width, gfx.height)
-    }
-    gfx.colorMode(this.pg.HSL, 360, 100, 100)
-    gfx.stroke(0, 0, 20)
-    gfx.strokeWeight(this.pg.random(1.5, 3))
-    gfx.noFill()
-    gfx.push()
-    const center = this.pg.createVector(gfx.width / 2, gfx.height / 2)
-    gfx.translate(center.x, center.y)
-    gfx.rotate(this.pg.random(0, this.pg.TWO_PI))
-    gfx.beginShape()
-    for(let i = 0; i < scratchLength; i++) {
-      const noiseVal = this.pg.noise(i*.1)
-
-      gfx.curveVertex(i * this.pg.random(-5, 5), noiseVal * 100)
-    }
-    gfx.endShape()
-    gfx.filter(this.pg.BLUR, this.pg.random(2, 4))
-    gfx.pop()
-    return gfx
-  }
-
-  drawSpot(placement) {
-    const gfx = this.createGraphics(this.pg.width * .2, this.pg.height * .2)
-    if (this.isDebugging) {
-      this.pg.noFill()
-      this.pg.stroke(0, 100, 50)
-      this.pg.rect(placement.x, placement.y, gfx.width, gfx.height)
-    }
-    gfx.colorMode(this.pg.HSL, 360, 100, 100)
-    gfx.fill(0, 0, 20)
-    gfx.noStroke()
-    gfx.push()
-    const eWidth = this.pg.random(3, 10)
-    const eHeight = eWidth * this.pg.random(.8, 1.2)
-    const center = this.pg.createVector(gfx.width / 2, gfx.height / 2)
-    gfx.translate(center.x, center.y)
-    gfx.rotate(this.pg.random(0, this.pg.TWO_PI))
-    gfx.ellipse(0, 0, eWidth, eHeight)
-    gfx.filter(this.pg.BLUR, this.pg.random(2, 4))
-    gfx.pop()
-    return gfx
   }
 
   drawNoise() {
